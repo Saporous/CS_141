29b97d94678d981597117fb1d4f76947c494e49b
Jeffrey C <chen.jeffrey.r@gmail.com>
2015-04-22 17:30:20 -0700

    modify assn1 distance formula

--- assignment1/assn1.py
+++ assignment1/assn1.py
@@ -10,16 +10,20 @@ class Point:
         self.y = y
     def __str__(self):
         return "%s, %s" % (self.x, self.y)
-def distance(a, b):
-    return sqrt(pow(a.x-b.x,2)+pow(a.y-b.y,2))
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt(pow(x1 - x2,2) + pow(y1 - y2,2))
 
 def printPoint():
     print('X: ' + self.x + ' Y: ' + self.y)
 
 def parse():
+    arr = []
     # open file
     with open(str(sys.argv[1]), 'r') as file:
         temp = file.read()
+        # print(temp)
+        print('\nthere are %s datas' % (len(temp.split())))
         file.close()
 
     # convert string into a list
@@ -27,12 +31,9 @@ def parse():
     i = 0
     # convert coordinates into points
     while i < len(temp):
-        pointArr.append(Point(temp[i], temp[i+1]))
+        arr.append(Point(float(temp[i]), float(temp[i+1])))
         i += 2
-
-pointArr = []
-parse()
-print(pointArr[0])
+    return arr
 
 def merge_sort(k):
     if len(k) == 1
@@ -54,3 +55,9 @@ def merge(l, r)
             seq[k] = r[j]
             j++
         k++
+
+pointArr = parse()
+print('done parsing\n')
+print('there are %s points to compare' % (len(pointArr)))
+shortestDistance = merge_sort(pointArr)
+print(shortestDistance)
